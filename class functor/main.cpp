8480b4bfad1c68ff0edf7d8c7633ac1630803a7b
蔡伟 <1607575130@qq.com>
2021-04-18 00:20:08 +0800

    update

---
+++ class functor/main.cpp
@@ -0,0 +1,53 @@
+﻿// head
+#include <iostream>
+#include <fstream>
+#include <algorithm>
+#include <list>
+
+// class
+class AddValue {
+private:
+	int theValue;
+public:
+	// constructor initializes the value to add
+	AddValue ( int v ) :theValue ( v ) {
+		return;
+	}
+
+	// the "function call" for the element adds the value
+	void operator()( int& elem )const {
+		elem += theValue;
+	}
+};
+
+// main
+int main () {
+	// list
+	std::list<int>coll;
+
+	// insert elements from 1 to 9
+	for ( int i = 1; i <= 9; ++i ) {
+		coll.push_back ( i );
+	}
+
+	// print
+	std::cout << "initialized:\n";
+	for ( int x : coll ) {
+		std::cout << x << " ";
+	}
+	std::cout << std::endl;
+
+	// add value 10 to each element
+	std::for_each ( coll.begin () , coll.end () , AddValue ( 10 ) );
+
+	// print
+	std::cout << "initialized:\n";
+	for ( int x : coll ) {
+		std::cout << x << " ";
+	}
+	std::cout << std::endl;
+
+	// exit
+	system ( "PAUSE" );
+	std::exit ( EXIT_SUCCESS );
+}
---
+++ functor/main.cpp
@@ -0,0 +1,41 @@
+﻿// head
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <algorithm>
+
+// square
+template<typename T = int>
+void func ( T& para_x ) {
+	para_x *= para_x;
+	return;
+}
+
+// main
+int main () {
+	// vector
+	std::vector<int>vec;
+
+	// file
+	std::fstream fout ( "out.txt" , std::ios::out | std::ios::app );
+
+	// insert value from 1 to 9
+	for ( int i = 1; i <= 9; ++i ) {
+		vec.push_back ( i );
+	}
+
+	// print
+	std::copy ( vec.begin () , vec.end () , std::ostream_iterator<int> ( fout , " " ) );
+	fout << std::endl;
+
+	// for each
+	std::for_each ( vec.begin () , vec.end () , func<int> );
+
+	// print
+	std::copy ( vec.begin () , vec.end () , std::ostream_iterator<int> ( fout , " " ) );
+	fout << std::endl;
+
+	// exit
+	fout.close ();
+	std::exit ( EXIT_SUCCESS );
+}
