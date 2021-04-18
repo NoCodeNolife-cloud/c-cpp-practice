98e2d8b34b09005f189fc546e7d3bbbf1cf206b1
蔡伟 <1607575130@qq.com>
2021-04-18 16:30:46 +0800

    update

---
+++ smart pointers/main.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <memory>
+
+namespace cod {
+	class Test {
+	private:
+		std::shared_ptr<char> shared_ptr_one;
+	public:
+		Test ( std::shared_ptr<char> para_shared_ptr_one ) {
+			shared_ptr_one = para_shared_ptr_one;
+		}
+
+		void func () {
+			*shared_ptr_one = 'j';
+		}
+	};
+}
+
+int main () {
+	std::shared_ptr<char> ptr;
+	ptr.reset ( new char ( 'k' ) );
+
+	cod::Test c_test ( ptr );
+	c_test.func ();
+
+	std::cout << *ptr << std::endl;
+
+	system ( "PAUSE" );
+	return 0;
+}
