e3128a9882bd55df44ef5fc21495f8e8baed77b3
蔡伟 <1607575130@qq.com>
2021-04-21 19:45:01 +0800

    update

---
+++ string set/main.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <string>
+#include <algorithm>
+#include <set>
+#include <fstream>
+
+template <typename T>
+struct cmp {
+	bool operator()( const T& _left , const T& _right ) const {
+		return _left < _right;
+	}
+};
+
+int main () {
+	// open file
+	std::fstream fout ( "out.txt" , std::ios::app );
+
+	// assign
+	std::set<std::string , cmp<std::string>>coll ( ( std::istream_iterator<std::string> ( std::cin ) ) , ( std::istream_iterator<std::string> () ) );
+
+	// print
+	std::copy ( coll.begin () , coll.end () , std::ostream_iterator<std::string> ( fout , "\n" ) );
+
+	// close file
+	fout.close ();
+
+	// exit
+	system ( "PUASE" );
+	std::exit ( EXIT_SUCCESS );
+}
---
+++ vector sort/main.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <fstream>
+#include <algorithm>
+#include <string>
+#include <vector>
+
+template <typename T>
+struct cmp {
+	bool operator()( const T& _left , const T& _right ) {
+		return _left < _right;
+	}
+};
+
+int main () {
+	// open file
+	std::fstream fout ( "out.txt" , std::ios::app );
+
+	// create a string vector
+	std::vector<std::string>coll ( ( std::istream_iterator<std::string> ( std::cin ) ) , std::istream_iterator<std::string> () );
+
+	// sort
+	std::sort ( coll.begin () , coll.end () , cmp<std::string> () );
+
+	//print
+	std::copy ( coll.begin () , coll.end () , std::ostream_iterator<std::string> ( fout , "\n" ) );
+
+	//exit
+	std::exit ( EXIT_SUCCESS );
+}
