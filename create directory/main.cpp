d45b5d24d9b46ca359b4c1e12356f568682019f7
蔡伟 <1607575130@qq.com>
2021-04-23 17:09:48 +0800

    update

---
+++ create directory/main.cpp
@@ -0,0 +1,15 @@
+﻿#include <fstream>
+#include <string>
+#include <iostream>
+
+int main () {
+	// print
+	std::cout << "enter the file name \n";
+	std::string str;
+	std::getline ( std::cin , str );// get the line
+
+	std::fstream fout ( str , std::ios::out );
+
+	// exit
+	std::exit ( EXIT_SUCCESS );
+}
