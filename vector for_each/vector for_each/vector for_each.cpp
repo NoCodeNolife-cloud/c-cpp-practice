865eb803143bb5cf7836f4eba006b0b8a55a5618
蔡伟 <1607575130@qq.com>
2021-04-26 16:57:10 +0800

    update

---
+++ vector for_each/vector for_each/vector for_each.cpp
@@ -0,0 +1,33 @@
+﻿#include <vector>
+#include <iostream>
+
+int main () {
+	srand ( time ( NULL ) );// random
+	int arr [ 10 ] = {  };// declare array
+
+	// initialize
+	for ( int& itm : arr ) {
+		itm = rand () % 100;
+	}
+
+	// print
+	for ( int itm : arr ) {
+		std::cout << itm << " ";
+	}
+	std::cout << std::endl;
+
+	std::vector<int>vec ( 10 );// declare vector
+
+	// initialize
+	for ( int& itm : vec ) {
+		itm = rand () % 1000;
+	}
+
+	// print
+	for ( int itm : vec ) {
+		std::cout << itm << " ";
+	}
+	std::cout << std::endl;
+
+	system ( "pause" );
+}
