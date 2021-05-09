44f146d5f737b8e6e7927adb049703bb72fea4fc
蔡伟 <1607575130@qq.com>
2021-05-09 09:42:08 +0800

    update

---
+++ vector swap/vector swap/vector swap.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+#include <vector>
+#include <iomanip>
+#include <algorithm>
+
+int main () {
+	std::vector<int>coll1 ( 10 );
+	std::vector<int>coll2 ( 10 );
+
+	std::cout << "coll1: ";
+	for ( int i = 0; i < 10; ++i ) {
+		coll1 [ i ] = i;
+		std::cout << std::setw ( 5 ) << coll1 [ i ];
+	}
+	std::cout << std::endl;
+
+	std::cout << "coll2: ";
+	for ( int i = 0; i < 10; ++i ) {
+		coll2 [ i ] = i + 10;
+		std::cout << std::setw ( 5 ) << coll2 [ i ];
+	}
+	std::cout << std::endl;
+
+	std::swap ( coll1 , coll2 );
+
+	std::cout << "coll1: ";
+	for ( int item : coll1 ) {
+		std::cout << std::setw ( 5 ) << item;
+	}
+	std::cout << std::endl;
+
+	std::cout << "coll2: ";
+	for ( int item : coll2 ) {
+		std::cout << std::setw ( 5 ) << item;
+	}
+	std::cout << std::endl;
+
+	system ( "pause" );
+}
