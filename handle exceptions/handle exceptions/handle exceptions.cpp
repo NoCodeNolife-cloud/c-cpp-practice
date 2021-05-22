80d32640acfe2b093df25edf49226ac0c9ad1dfc
蔡伟 <1607575130@qq.com>
2021-05-22 09:49:33 +0800

    update

---
+++ handle exceptions/handle exceptions/handle exceptions.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+double division ( double& x , double& y ) {
+
+	if ( y == 0 ) {
+
+		throw "can't division zero";
+	} else {
+
+		return x / y;
+	}
+}
+
+int main () {
+
+	double a = 5;
+	double b = 0;
+
+	try {
+
+		cout << division ( a , b );
+	} catch ( const char argc [] ) {// or const char* argc
+
+		cout << argc << endl;
+	}
+
+	system ( "pause" );
+}
