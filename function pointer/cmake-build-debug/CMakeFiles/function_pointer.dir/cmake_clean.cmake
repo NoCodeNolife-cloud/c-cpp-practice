e435ed589ae3e9f98872dd9e152c7270f9998eca
落奈 <54143278+NoCodeNolife-cloud@users.noreply.github.com>
2021-07-07 17:57:19 +0800

    update

---
+++ function pointer/cmake-build-debug/CMakeFiles/function_pointer.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/function_pointer.dir/main.cpp.obj"
+  "function_pointer.exe"
+  "function_pointer.exe.manifest"
+  "function_pointer.pdb"
+  "libfunction_pointer.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/function_pointer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
